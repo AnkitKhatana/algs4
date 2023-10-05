3ee63c41346365038bc63cc88fd0e2cb76acf410
AnkitKhatana <kank2610@gmail.com>
2023-10-05 17:50:32 +0530

    Fix to resolve import errors in intelllIj

--- src/edu/princeton/cs/algs4/DrawListener.java
+++ src/edu/princeton/cs/algs4/DrawListener.java
@@ -61,7 +61,7 @@ public interface DrawListener {
      * A mouse click is triggered only if the user presses a mouse button
      * and then releases it quickly, without moving the mouse.
      * It does not work with touch events.
-     * The {@link mousePressed} method is generally preferred for
+     * The { mousePressed} method is generally preferred for
      * detecting mouse clicks.
      *
      * @param x the x-coordinate of the mouse
